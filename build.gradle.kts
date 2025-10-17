f9bae0ec4c217bd632c5448b79076dca0e0e16d6
Lukas <contact@lukasbreuer.de>
2025-10-17 18:42:50 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,34 @@
 plugins {
-    id("java")
+  id("java")
 }
 
 group = "de.lukasbreuer"
 version = "1.0.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation(platform("org.junit:junit-bom:5.10.0"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+  implementation("com.google.inject:guice:7.0.0")
+
+  implementation("com.google.guava:guava:33.5.0-jre")
+
+  implementation("org.projectlombok:lombok:1.18.42")
+  annotationProcessor("org.projectlombok:lombok:1.18.42")
+  testImplementation("org.projectlombok:lombok:1.18.42")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
+
+  implementation("com.datastax.oss:java-driver-core:4.17.0")
+
+  implementation("org.json:json:20250517")
+  implementation("commons-io:commons-io:2.20.0")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
